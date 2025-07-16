59334101fcba3cdc05987fe323e8ad6352a8ad76
N0Xl0US <g4lnoxious@gmail.com>
2025-07-16 05:47:18 -0700

    Add /ping route to keep_alive.py for health check functionality

--- keep_alive.py
+++ keep_alive.py
@@ -8,6 +8,10 @@ app = Flask('')
 def home():
     return "Bot is alive!"
 
+@app.route('/ping')
+def ping():
+    return "pong"
+
 def run():
     port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
